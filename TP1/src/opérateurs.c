6cd99fe4c2e003feb3fbc8d0cd5c417462172f5a
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-03 20:58:07 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -5,15 +5,16 @@ int main() {
 
   int a = 16;
   int b = 3;
-  printf( "%d + %d == %d\n", a, b, a+b );
-  printf( "%d - %d == %d\n", b, a, b-a );
-  printf( "%d * %d == %d\n", a, b, a*b );
-  printf( "%d / %d == %d\n", a, b, a/b );
-  printf( "%d module %d == %d\n", a, b, a%b );
-  printf( "%d == %d == %d\n", a, b, (a==b) );
-  printf( "%d > %d == %d\n", a, b, (a>b) );
+  printf( "%d + %d = %d\n", a, b, a+b );
+  printf( "%d - %d = %d\n", b, a, b-a );
+  printf( "%d * %d = %d\n", a, b, a*b );
+  printf( "%d / %d = %d\n", a, b, a/b );
+  printf( "%d module %d = %d\n", a, b, a%b );
+  printf( "%d == %d ==> %d\n", a, b, (a==b) );
+  printf( "%d > %d ==> %d\n", a, b, (a>b) );
  
 }
+
 
 
 
