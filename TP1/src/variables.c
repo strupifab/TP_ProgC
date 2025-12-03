d7b7e4e1b9e76ae814a65b5694296cc1ab813541
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-03 20:42:10 +0100

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -4,33 +4,30 @@
 int main() {
 
   char *charvar = "ma charvar";
-  short *shortvar = "ma shortvar";
-  inf intvar = 12
-  signed int signedintvar = -80
-  unsigned int unsignedintvar = 300
-
-  signed long int signedlongintvar = -500000
-  unsigned long int	unsignedlongintvar = 5055555
-
-  signed long long int signedlonglongintvar = -50000000
-  unsigned long long int unsignedlonglongintvar = 5055555666
-
-  float floatvar = 3.55
-  double doublevar = 6
-  long double doubledoublevar = 6000
+  short shortvar = 10;
+  inf intvar = 12;
+  signed int signedintvar = -80;
+  unsigned int unsignedintvar = 300;
+  signed long int signedlongintvar = -500000;
+  unsigned long int	unsignedlongintvar = 5055555;
+  signed long long int signedlonglongintvar = -50000000;
+  unsigned long long int unsignedlonglongintvar = 5055555666;
+  float floatvar = 3.55;
+  double doublevar = 6;
+  long double doubledoublevar = 6000;
 
   printf("charvar = %s", charvar);
-  printf("shortvar = %s", shortvar);
-  printf("intvar = %i", intvar);
-  printf("signedintvar = %i", signedintvar);
-  printf("unsignedintvar = %i", unsignedintvar);
-  printf("signedlongintvar = %i", signedlongintvar);
-  printf("unsignedlongintvar = %i", unsignedlongintvar);
-  printf("signedlonglongintvar = %i", signedlonglongintvar);
-  printf("unsignedlonglongintvar = %i", unsignedlonglongintvar);
-
-  printf("floatvar = %f",floatvar);
-  printf("doublevar = %f",doublevar);
-  printf("doubledoublevar = %f",doubledoublevar);
+  printf("\nshortvar = %i", shortvar);
+  printf("\nintvar = %i", intvar);
+  printf("\nsignedintvar = %i", signedintvar);
+  printf("\nunsignedintvar = %i", unsignedintvar);
+  printf("\nsignedlongintvar = %li", signedlongintvar);
+  printf("\nunsignedlongintvar = %li", unsignedlongintvar);
+  printf("\nsignedlonglongintvar = %lli", signedlonglongintvar);
+  printf("\nunsignedlonglongintvar = %lli", unsignedlonglongintvar);
+  printf("\nfloatvar = %f",floatvar);
+  printf("\ndoublevar = %f",doublevar);
+  printf("\ndoubledoublevar = %lf",doubledoublevar);
 
 }
+
