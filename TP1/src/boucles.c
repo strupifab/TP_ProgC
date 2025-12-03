0322fae95356747ef8a3ad4611b180218da072a6
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-03 21:29:12 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,18 +4,24 @@
 int main() {
 
   int compteur = 5 ;
-  if ( compteur > 10 ) break;
+  if ( compteur < 10 ) {
 
-  // Boucle for
-  for (int i = 0; i < compteur; ++i) {
-    for (int j = 0; j <= i; ++j) {
-      //printf("*");
-      if (i >= 2 && i < (compteur-1) &&  j >= 1 && j < i) printf("#");
-      else printf("*");
-     }
-    printf("\n");
-  }
+    // Boucle for
+    // **************************
+    for (int i = 0; i < compteur; ++i) {
+      for (int j = 0; j <= i; ++j) {
+        //printf("*");
+        if (i >= 2 && i < (compteur-1) &&  j >= 1 && j < i) printf("#");
+        else printf("*");
+       }
+      printf("\n");
+    }
+    // **************************
 
- 
+ }
+  else
+    printf("Erreur : compteur > 10");
+
   
 }
+
