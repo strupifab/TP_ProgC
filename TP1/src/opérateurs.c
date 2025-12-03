8f46e0a6e5d4a9b7dc16a8c265a7f2cd35ef7ac3
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-03 20:58:43 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -10,10 +10,11 @@ int main() {
   printf( "%d * %d = %d\n", a, b, a*b );
   printf( "%d / %d = %d\n", a, b, a/b );
   printf( "%d module %d = %d\n", a, b, a%b );
-  printf( "%d == %d ==> %d\n", a, b, (a==b) );
-  printf( "%d > %d ==> %d\n", a, b, (a>b) );
+  printf( "%d == %d ? %d\n", a, b, (a==b) );
+  printf( "%d > %d ? %d\n", a, b, (a>b) );
  
 }
+
 
 
 
