ee6b0cb8539fc2b2e44b871adb4d3c734f35011d
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-17 21:22:24 +0100

    Update bits.c

---
+++ TP2/src/bits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/*
+Vérification des Bits en C
+
+L'objectif de cet exercice est d'écrire un programme en C nommé bits.c qui vérifie si les 4ème et 20ème bits de gauche d'une variable entière d sont à 1 en représentation binaire. 
+Si les deux bits sont 1, le programme doit afficher 1 ; sinon, il doit afficher 0. 
+Vous devez réaliser cette vérification sans utiliser de fonctions et en plaçant toutes les instructions à l'intérieur de la fonction main().
+*/
+
+int main() {
+
+  int d = 1235;
+
+  
+}
