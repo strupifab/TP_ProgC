6b12a1a426a395d2716b667ba7093b42456e3fd9
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-17 21:04:56 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -12,9 +12,8 @@ Exemple pour n = 5
 */
 
 int main() {
-
-  int n = 10;
-  
+  int n = 5;
+  printf("Début de la génération de la pyramide de taille %d \n", n);
   for (int i=1;i<=n;i++) {
 
       //printf("Num ligne = %d\n",i);
@@ -30,5 +29,6 @@ int main() {
 
    printf( "\n");   
   }
-  
+  printf("\n Génération de la pyramide est terminée !");
+    
 }
