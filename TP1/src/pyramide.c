9208ac8854f0414ae64ad446a8270cf7d623940f
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-17 20:37:04 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+/*
+Génération d'une pyramide de nombres
+Objectif : Écrivez un programme nommé pyramide.c pour générer et afficher une pyramide de nombres en utilisant des boucles imbriquées. Ce motif utilise des nombres dans un motif spécifique.
+Exemple pour n = 5
+    1
+   121
+  12321
+ 1234321
+123454321
 
+*/
+
+int main() {
+
+  int n = 5;
+  
+  for (int i=0;i<n;i++) {
+     printf("%d\n",i);
+  }
+
+
+    /*
+      for (int i=0;i<=sizeof numbinaire / sizeof *numbinaire;i++) {
+        numbinaire[i] = -1;
+      }
+ */
+  
+  //printf("\nConversion de %d en binaire = ", numdecimal);
+  //printf( "\n");
+}
