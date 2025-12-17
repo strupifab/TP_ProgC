4d8e59ef753654ce3d22657500d6ffeb43a32343
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-17 20:28:56 +0100

    Update binaire.c

--- TP1/src/binaire.c
+++ TP1/src/binaire.c
@@ -15,13 +15,43 @@ Suggestions pour le programme :
 
 Pensez à utiliser les opérateurs pour la manipulation de bits (&, | ou ~)
 */
+
 int main() {
+  int numdecimalTab[] = {0,4096,65536,65535,1024};
 
-  int num1 = 10;
-  //int num1 = 0b1010101;
-  printf( "%d\n", num1[0]);  
-  printf( "~%d = %d\n", num1, ~num1);
-  printf("\n");
+  for (int num=0;num<5;num++) {
 
+      int numdecimal = numdecimalTab[num];
+      unsigned int numbinaire[20];
+      int reste = numdecimal % 2;
+      int resultat = numdecimal / 2;
+      int cpt = 0;
+    
+      for (int i=0;i<=sizeof numbinaire / sizeof *numbinaire;i++) {
+        numbinaire[i] = -1;
+      }
+    
+      numbinaire[cpt] = reste;
+    
+      while (resultat != 0){
+    
+         int numdecimaltmp = resultat;
+         reste = numdecimaltmp % 2;
+         resultat = numdecimaltmp / 2;
+    
+         //printf( "%d / %d = %d\n", numdecimaltmp, 2, resultat);
+         //printf( "%d modulo %d = %d\n", numdecimaltmp, 2, reste);
+    
+         cpt ++;
+         numbinaire[cpt] = reste;
+      }
+    
+      printf("\nConversion de %d en binaire = ", numdecimal);
+      for (int i=20;i>=0;i--) {
+        if ( numbinaire[i] != -1) printf("%d", numbinaire[i]);
+      }
+
+  }
+
+  printf( "\n");
 }
-
