8679a6e11239deb2cb83675ff5b44b79d4852695
strupifab <43041282+strupifab@users.noreply.github.com>
2025-12-17 18:07:44 +0100

    Update conditions.c

--- TP1/src/conditions.c
+++ TP1/src/conditions.c
@@ -23,13 +23,14 @@ int main() {
     if ( (i % 5 == 0) || ( i % 7 == 0) ) somme += i;
     else if (i % 11 == 0 ) continue;
     else if ( somme > 5000 ) break;
-    printf("\n Compteur = %i", i);
+    printf("\n Compteur = %i et somme = %somme", i,somme);
   }
   printf("\n----------------------");
-  printf("\nSomme = %i", somme);
+  printf("\nSomme = %i", somme );
   printf("\n");
 
 }
+
 
 
 
